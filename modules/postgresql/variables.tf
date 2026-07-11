3f43ed168ba4a0d0ac8f029b4d498a77b385d31c
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-07-12 01:45:04 +0300

    Swap db_username and db_password variables

--- modules/postgresql/variables.tf
+++ modules/postgresql/variables.tf
@@ -1,11 +1,22 @@
-variable "db_username" {
-  default = "postgres"
+variable "db_password" {
+  description = "Master password for PostgreSQL"
+  type        = string
+  sensitive   = true
 }
 
-variable "db_password" {
-  sensitive = true
+variable "db_username" {
+  description = "Master username"
+  type        = string
+  default     = "postgres"
 }
 
 variable "db_name" {
-  default = "appdb"
+  description = "Initial database name"
+  type        = string
+  default     = "postgres"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
