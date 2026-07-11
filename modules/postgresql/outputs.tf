36efaa0c7b6add0ff4df53d5b0551e5c0aa2c24f
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-07-12 01:43:54 +0300

    Update outputs.tf

--- modules/postgresql/outputs.tf
+++ modules/postgresql/outputs.tf
@@ -2,13 +2,16 @@ output "endpoint" {
   value = aws_db_instance.postgres.address
 }
 
+
 output "port" {
   value = aws_db_instance.postgres.port
 }
 
+
 output "database" {
   value = aws_db_instance.postgres.db_name
 }
+
 
 output "username" {
   value = aws_db_instance.postgres.username
